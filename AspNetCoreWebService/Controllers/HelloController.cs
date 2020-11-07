e052c9f0cd24555022b981551f9492c40e1e6386
lian.guanhua <lianstorm@gmail.com>
2020-11-07 20:25:50 +0800

    update echo

--- AspNetCoreWebService/Controllers/HelloController.cs
+++ AspNetCoreWebService/Controllers/HelloController.cs
@@ -9,7 +9,7 @@ namespace AspNetCoreWebService.Controllers
     [Route("/")]
     public class HelloController : Controller
     {
-        private const string MESSAGE_FORMAT = "Hello {0}!！";
+        private const string MESSAGE_FORMAT = "Hello {0}!";
 
         [HttpGet]
         public JsonResult Get([FromQuery] string name = "World")
